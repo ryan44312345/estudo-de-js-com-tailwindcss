1946a5fc1f0f6b66c6fbfbe4d9172eb3d3ac6fef
Ryan Lucas <ryanlucasstos@gmail,com>
2025-08-09 18:14:51 -0300

    versão com carrossel

--- darkmode/darkmode.js
+++ darkmode/darkmode.js
@@ -9,6 +9,11 @@ const menu2 = document.getElementById('aside2')
 const avisotema = document.getElementById('modal')
 const sim = document.getElementById('yes')
 const nao = document.getElementById('no')
+const img = document.getElementById('imagem')
+const carrossel = document.getElementById('carrossel')
+const imagens = ["img/a9940d849e126607d13c11801dd860ff.jpg", 'img/funny-dog-picture-figc088ef31ji2q6.webp', "img/funny-dog-picture-j1uewwx9yy5v7okm.webp"]
+let contador = 0
+let intervalo;
 
 trc.addEventListener('click', () => {
     avisotema.classList.toggle('hidden')
@@ -51,3 +56,22 @@ botaomenu2.addEventListener('click', () => {
     menu2.classList.toggle('hidden')
     menu.classList.toggle('-translate-x-full')
 })
+
+function trocarImagem() {
+    contador = (contador += 1) % imagens.length
+    img.src = imagens[contador]
+}
+
+function iniciar() {
+    intervalo = setInterval(trocarImagem, 3000)
+}
+
+function pararContagem() {
+    clearInterval(intervalo)
+}
+
+iniciar()
+
+carrossel.addEventListener("mouseenter", pararContagem)
+
+carrossel.addEventListener("mouseleave", iniciar)
