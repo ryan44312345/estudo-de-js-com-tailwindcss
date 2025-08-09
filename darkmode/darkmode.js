f00d9e7ddc7ab4c0270b77983b68e242d009f03a
Ryan Lucas <ryanlucasstos@gmail,com>
2025-08-09 15:36:35 -0300

    Versão com darkmode/lightmode, sidebar e aviso de confirmação

--- darkmode/darkmode.js
+++ darkmode/darkmode.js
@@ -1,6 +1,53 @@
 const trc = document.getElementById('troca')
 const corpo = document.getElementById('body')
+const corpo2 = document.getElementById('body')
+const caixa = document.getElementById('box')
+const botaomenu = document.getElementById('asidebutton')
+const menu = document.getElementById('aside')
+const botaomenu2 = document.getElementById('asidebutton2')
+const menu2 = document.getElementById('aside2')
+const avisotema = document.getElementById('modal')
+const sim = document.getElementById('yes')
+const nao = document.getElementById('no')
 
 trc.addEventListener('click', () => {
-    corpo.classList.toggle('dark')
+    avisotema.classList.toggle('hidden')
+})
+
+sim.addEventListener('click', () => {
+    corpo.classList.toggle('bg-black')
+    corpo.classList.toggle('bg-white')
+    corpo.classList.toggle('text-white')
+    corpo.classList.toggle('text-black')
+    trc.classList.toggle('text-white')
+    trc.classList.toggle('bg-slate-100')
+    trc.classList.toggle('bg-slate-950')
+    caixa.classList.toggle('bg-slate-950')
+    caixa.classList.toggle('bg-slate-300')
+    caixa.classList.toggle('border-black')
+    botaomenu2.classList.toggle('bg-slate-950')
+    botaomenu2.classList.toggle('bg-white')
+    botaomenu2.classList.toggle('text-black')
+    botaomenu2.classList.toggle('text-white')
+    botaomenu.classList.toggle('bg-slate-950')
+    botaomenu.classList.toggle('bg-white')
+    botaomenu.classList.toggle('text-black')
+    botaomenu.classList.toggle('text-white')
+    menu.classList.toggle('bg-slate-400')
+    menu.classList.toggle('bg-slate-900')
+    avisotema.classList.toggle('hidden')
+})
+
+nao.addEventListener('click', () => {
+    avisotema.classList.toggle('hidden')
+})
+
+botaomenu.addEventListener('click', () => {
+    menu.classList.toggle('-translate-x-full')
+    menu2.classList.toggle('hidden')
+})
+
+botaomenu2.addEventListener('click', () => {
+    menu2.classList.toggle('hidden')
+    menu.classList.toggle('-translate-x-full')
 })
